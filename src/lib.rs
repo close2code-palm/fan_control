b2032c2984a3e26710c501c73d3ec38f354b7943
Juxan <julyfortune101@gmail.com>
2024-06-02 00:31:46 +0300

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use libc::{EINVAL, pid_t, regex_t};
+use libc::{EINVAL, pid_t};
 use redhook::{hook, real};
 use std::process::Command;
 
@@ -10,13 +10,10 @@ unsafe fn fanotify_mark(
     dirfd: libc::c_int,
     path: *const libc::c_char
 ) -> libc::c_int => fan_stub {
-    // let cs = CString::new("read_events").unwrap();
     println!("marking...");
     Command::new("wall").arg("Someone is checking...").spawn().expect("wall fail");
-    // let hooked = unsafe { dlsym(RTLD_NEXT,  cs.as_ptr())};
-    // sleep(Duration::from_secs(10));
+
     return EINVAL;
-        // getpid()
 }
 }
 
@@ -24,7 +21,7 @@ hook! {
     unsafe fn getpid() -> pid_t => gp_stub {
         println!("Your about to know everything!");
         let res = real!(getpid)();
-        println!("reality: {res}")
+        println!("reality: {res}");
         return res + 2
     }
 }
