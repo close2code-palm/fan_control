1b7fbbe2d0aa21a5efd192d015817c1ad4b8d961
Juxan <julyfortune101@gmail.com>
2024-06-01 20:37:01 +0300

    overwrited interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
+use nix::sys::fanotify::FanotifyEvent;
 use std::ffi::CString;
 use std::process::Command;
 use std::thread::sleep;
 use std::time::Duration;
-use nix::sys::fanotify::FanotifyEvent;
 
 #[cfg(target_os = "linux")]
-fn read_events() -> Vec<FanotifyEvent> {
-    let cs = CString::new("read_events").unwrap();
+pub unsafe extern "C" fn fanotify_mark(
+    fd: libc::c_int,
+    flags: libc::c_uint,
+    mask: u64,
+    dirfd: libc::c_int,
+    path: *const libc::c_char,
+) -> Vec<FanotifyEvent> {
+    // let cs = CString::new("read_events").unwrap();
     Command::new("wall").arg("Someone is checking...");
     // let hooked = unsafe { dlsym(RTLD_NEXT,  cs.as_ptr())};
     sleep(Duration::from_secs(10));
     return vec![];
 }
-
