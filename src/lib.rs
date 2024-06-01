99aa6a2670be0873b17e32278841496ece3b3b8a
Juxan <julyfortune101@gmail.com>
2024-06-01 22:07:37 +0300

    prepared solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
 use std::process::Command;
-use std::thread::sleep;
-use std::time::Duration;
 use libc::EINVAL;
+use redhook::hook;
 
-#[cfg(target_os = "linux")]
-pub unsafe extern "C" fn fanotify_mark(
+hook!{
+unsafe fn fanotify_mark(
     fd: libc::c_int,
     flags: libc::c_uint,
     mask: u64,
     dirfd: libc::c_int,
-    path: *const libc::c_char,
-) -> libc::c_int {
+    path: *const libc::c_char
+) -> libc::c_int => fan_stub {
     // let cs = CString::new("read_events").unwrap();
     println!("marking...");
     Command::new("wall").arg("Someone is checking...").spawn().expect("wall fail");
     // let hooked = unsafe { dlsym(RTLD_NEXT,  cs.as_ptr())};
     // sleep(Duration::from_secs(10));
     return EINVAL;
+}
 }
