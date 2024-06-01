6e45d0416665ede6f7baa119a8ee02d90692b867
Juxan <julyfortune101@gmail.com>
2024-06-01 20:21:00 +0300

    stub

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 use std::ffi::CString;
-use libc::{dlsym, RTLD_NEXT};
+use std::process::Command;
+use std::thread::sleep;
+use std::time::Duration;
 use nix::sys::fanotify::FanotifyEvent;
 
 #[cfg(target_os = "linux")]
 fn read_events() -> Vec<FanotifyEvent> {
     let cs = CString::new("read_events").unwrap();
-    let hooked = unsafe { dlsym(RTLD_NEXT,  cs.as_ptr())};
+    Command::new("wall").arg("Someone is checking...");
+    // let hooked = unsafe { dlsym(RTLD_NEXT,  cs.as_ptr())};
+    sleep(Duration::from_secs(10));
     return vec![];
 }
 
